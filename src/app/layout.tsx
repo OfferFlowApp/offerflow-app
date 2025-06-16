27f2acbdf315adaa901192516fb537e6fcedd52b
Giorgaras Dimitris <giorgarasdimitrios@gmail.com>
2025-06-16 17:08:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased min-h-screen flex flex-col" suppressHydrationWarning={true}>
+      <body className="font-body antialiased min-h-screen flex flex-col">
         <AuthProvider> {/* Wrap AppProviders with AuthProvider */}
           <AppProviders>
             {children}
