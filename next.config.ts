756c74ebe81c890e8072b57b23b4e0fab546fa75
Giorgaras Dimitris <giorgarasdimitrios@gmail.com>
2025-07-03 17:10:30 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,12 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    // This is to suppress the harmless warning from handlebars, a dependency of genkit.
+    // Webpack 5 doesn't support `require.extensions`, but the code works fine otherwise.
+    config.module.exprContextCritical = false;
+    return config;
+  },
 };
 
 export default withPWA(nextConfig);
