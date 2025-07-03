32dfd06d967ec45096a2699f1be7a529ba8b26ac
Giorgaras Dimitris <giorgarasdimitrios@gmail.com>
2025-07-03 17:30:17 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/next.config.ts
+++ src/next.config.ts
@@ -6,18 +6,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
+        protocol: 'https,
         hostname: 'placehold.co',
         port: '',
         pathname: '/**',
       },
     ],
-  },
-  webpack: (config) => {
-    // This is to suppress the harmless warning from handlebars, a dependency of genkit.
-    // Webpack 5 doesn't support `require.extensions`, but the code works fine otherwise.
-    config.module.exprContextCritical = false;
-    return config;
   },
 };
 
