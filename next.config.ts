d43444c476e35b85e97f48f74629bd0f2d9f8524
Giorgaras Dimitris <giorgarasdimitrios@gmail.com>
2025-07-03 17:28:14 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,8 @@
 
 import type {NextConfig} from 'next';
-import * as nextPWA from "@ducanh2912/next-pwa";
-
-const withPWA = nextPWA.default({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
@@ -25,12 +13,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    // This is to suppress the harmless warning from handlebars, a dependency of genkit.
-    // Webpack 5 doesn't support `require.extensions`, but the code works fine otherwise.
-    config.module.exprContextCritical = false;
-    return config;
-  },
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
